4d677a4b744737c6939db325df27c777a59b5069
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2021-11-14 09:56:19 +0100

    Pushing Live

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # form-dummydata
-Dummy Data to fill your form
+Dummy Data to fill your form dynamically
+
+# Installation
+ `npm i cocodummyformdata --save` 
+
+Then ...
+
+```
+import { cocodummyformdata } from 'cocodummyformdata';
+
+cocodummyformdata({
+    input_type: 'name'
+})
+```
+
+
+## Options
+
+Dummy Form support 2 options, both of which are optional:
+
+* *input_type* - _name | (Defaults to value Coco Dummy)
---
+++ dummyform/index.js
@@ -0,0 +1,17 @@
+function cocodummyformdata (options) {
+    // pass input that have classes attached to them
+    let input_form = document.querySelectorAll('.cocodummyform');
+
+    //  options
+    if (options.input_type === 'name')
+         options.input_type = 'Samuel'
+    else
+        options.input_type = 'Coco Dummy'
+
+    input_form.forEach(input => {
+        input.style.boxShadow = `10px 10px 15px 1px rgba(0,0,0,0.12)`
+        input.value = `${options.input_type}`
+    })
+}
+
+module.exports.cocodummyformdata = cocodummyformdata;
